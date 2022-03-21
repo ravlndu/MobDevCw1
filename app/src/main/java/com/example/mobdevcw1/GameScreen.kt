e639b5c70ca968043e75b1c233ef426b74d89514
ravlndu <ravindu.20200303@iit.ac.lk>
2022-03-21 09:38:11 +0530

    error fixed commit

--- app/src/main/java/com/example/mobdevcw1/GameScreen.kt
+++ app/src/main/java/com/example/mobdevcw1/GameScreen.kt
@@ -3,26 +3,22 @@ package com.example.mobdevcw1
 import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
-import android.os.CountDownTimer
-import android.view.View
+
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import java.util.*
+
 
 
 class GameScreen : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
-    var gamestarted = false
-    lateinit var countDownTimer: CountDownTimer
-    var initialCountDown: Long=50000
-    var countdownInterval: Long=1000
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_screen)
 
-        val expression_left = findViewById<TextView>(R.id.exp1)
-        val expression_right = findViewById<TextView>(R.id.exp2)
+        val expressionLeft = findViewById<TextView>(R.id.exp1)
+        val expressionRight = findViewById<TextView>(R.id.exp2)
         val resultTxt = findViewById<TextView>(R.id.result)
         val clock = findViewById<TextView>(R.id.clock)
         val greaterBTN = findViewById<Button>(R.id.greaterBtn)
@@ -32,22 +28,22 @@ class GameScreen : AppCompatActivity() {
 
 
 
-        var finalquestionleft = FinalExpression()
-        var finalquestionright = FinalExpression()
+        var finalQuestionLeft = FinalExpression()
+        var finalQuestionRight = FinalExpression()
 
-        finalquestionleft.FinalQuestion()
-        finalquestionright.FinalQuestion()
-        println("total left -- ${finalquestionleft.total()}")
-        println("total right -- ${finalquestionright.total()}")
-        expression_left.text = finalquestionleft.questionstr()
-        expression_right.text = finalquestionright.questionstr()
+        finalQuestionLeft.finalQuestion()
+        finalQuestionRight.finalQuestion()
+        println("total left -- ${finalQuestionLeft.total()}")
+        println("total right -- ${finalQuestionRight.total()}")
+        expressionLeft.text = finalQuestionLeft.questionStr()
+        expressionRight.text = finalQuestionRight.questionStr()
 
 
 
 
 
         greaterBTN.setOnClickListener{
-            if (finalquestionleft.total()>finalquestionright.total()){
+            if (finalQuestionLeft.total()>finalQuestionRight.total()){
                 resultTxt.text = "Correct!"
                 resultTxt.setTextColor(Color.GREEN)
 
@@ -56,18 +52,18 @@ class GameScreen : AppCompatActivity() {
                 resultTxt.setTextColor(Color.RED)
 
             }
-            finalquestionleft = FinalExpression()
-            finalquestionright = FinalExpression()
-            finalquestionleft.FinalQuestion()
-            finalquestionright.FinalQuestion()
-            println("total left -- ${finalquestionleft.total()}")
-            println("total right -- ${finalquestionright.total()}")
-            expression_left.text = finalquestionleft.questionstr()
-            expression_right.text = finalquestionright.questionstr()
+            finalQuestionLeft = FinalExpression()
+            finalQuestionRight = FinalExpression()
+            finalQuestionLeft.finalQuestion()
+            finalQuestionRight.finalQuestion()
+            println("total left -- ${finalQuestionLeft.total()}")
+            println("total right -- ${finalQuestionRight.total()}")
+            expressionLeft.text = finalQuestionLeft.questionStr()
+            expressionRight.text = finalQuestionRight.questionStr()
 
         }
         lessBTN.setOnClickListener{
-            if (finalquestionleft.total()<finalquestionright.total()){
+            if (finalQuestionLeft.total()<finalQuestionRight.total()){
                 resultTxt.text = "Correct!"
                 resultTxt.setTextColor(Color.GREEN)
 
@@ -76,19 +72,19 @@ class GameScreen : AppCompatActivity() {
                 resultTxt.setTextColor(Color.RED)
 
             }
-            finalquestionleft = FinalExpression()
-            finalquestionright = FinalExpression()
-            finalquestionleft.FinalQuestion()
-            finalquestionright.FinalQuestion()
-            println("total left -- ${finalquestionleft.total()}")
-            println("total right -- ${finalquestionright.total()}")
-            expression_left.text = finalquestionleft.questionstr()
-            expression_right.text = finalquestionright.questionstr()
+            finalQuestionLeft = FinalExpression()
+            finalQuestionRight = FinalExpression()
+            finalQuestionLeft.finalQuestion()
+            finalQuestionRight.finalQuestion()
+            println("total left -- ${finalQuestionLeft.total()}")
+            println("total right -- ${finalQuestionRight.total()}")
+            expressionLeft.text = finalQuestionLeft.questionStr()
+            expressionRight.text = finalQuestionRight.questionStr()
 
 
         }
         equalBTN.setOnClickListener{
-            if (finalquestionleft.total()==finalquestionright.total()){
+            if (finalQuestionLeft.total()==finalQuestionRight.total()){
                 resultTxt.text = "Correct!"
                 resultTxt.setTextColor(Color.GREEN)
 
@@ -97,14 +93,14 @@ class GameScreen : AppCompatActivity() {
                 resultTxt.setTextColor(Color.RED)
 
             }
-            finalquestionleft = FinalExpression()
-            finalquestionright = FinalExpression()
-            finalquestionleft.FinalQuestion()
-            finalquestionright.FinalQuestion()
-            println("total left -- ${finalquestionleft.total()}")
-            println("total right -- ${finalquestionright.total()}")
-            expression_left.text = finalquestionleft.questionstr()
-            expression_right.text = finalquestionright.questionstr()
+            finalQuestionLeft = FinalExpression()
+            finalQuestionRight = FinalExpression()
+            finalQuestionLeft.finalQuestion()
+            finalQuestionRight.finalQuestion()
+            println("total left -- ${finalQuestionLeft.total()}")
+            println("total right -- ${finalQuestionRight.total()}")
+            expressionLeft.text = finalQuestionLeft.questionStr()
+            expressionRight.text = finalQuestionRight.questionStr()
 
 
         }
@@ -114,44 +110,44 @@ class GameScreen : AppCompatActivity() {
 
 }
 class FinalExpression{
-    var qelement:Int = (1..20).random()
-    var termcount:Int = (1..4).random()
-    var total:Int = qelement
-    var question_str = ""
-    var newelement = PrimaryExpression()
+    private var qElement:Int = (1..20).random()
+    private var termCount:Int = (1..4).random()
+    private var total:Int = qElement
+    private var questionString = ""
+    private var newElement = PrimaryExpression()
 
 
-    fun FinalQuestion(){
+    fun finalQuestion(){
 
-        when(termcount){
+        when(termCount){
             1 -> {
-                question_str = "$qelement"
-                total = qelement
+                questionString = "$qElement"
+                total = qElement
             }
             2 ->{
-                question_str = "$qelement"
-                total = newelement.primary_questiontotal(total)
-                question_str = question_str+ newelement.primary_questionstr()
-                total = newelement.primary_questiontotal(total)
+                questionString = "$qElement"
+                total = newElement.primaryQuestionTotal(total)
+                questionString += newElement.primaryQuestionString()
+                total = newElement.primaryQuestionTotal(total)
 
             }
             3 ->{
-                question_str = "$qelement"
-                total = newelement.primary_questiontotal(total)
-                question_str = "($qelement ${newelement.primary_questionstr()})"
-                total = newelement.primary_questiontotal(total)
-                question_str = question_str+ newelement.primary_questionstr()
-                total = newelement.primary_questiontotal(total)
+                questionString = "$qElement"
+                total = newElement.primaryQuestionTotal(total)
+                questionString = "($qElement ${newElement.primaryQuestionString()})"
+                total = newElement.primaryQuestionTotal(total)
+                questionString += newElement.primaryQuestionString()
+                total = newElement.primaryQuestionTotal(total)
             }
             4 ->{
-                question_str = "$qelement"
-                total = newelement.primary_questiontotal(total)
-                question_str = "(($qelement ${newelement.primary_questionstr()})"
-                total = newelement.primary_questiontotal(total)
-                question_str = question_str+"${newelement.primary_questionstr()})"
-                total = newelement.primary_questiontotal(total)
-                question_str = question_str+ newelement.primary_questionstr()
-                total = newelement.primary_questiontotal(total)
+                questionString = "$qElement"
+                total = newElement.primaryQuestionTotal(total)
+                questionString = "(($qElement ${newElement.primaryQuestionString()})"
+                total = newElement.primaryQuestionTotal(total)
+                questionString += "${newElement.primaryQuestionString()})"
+                total = newElement.primaryQuestionTotal(total)
+                questionString += newElement.primaryQuestionString()
+                total = newElement.primaryQuestionTotal(total)
             }
         }
     }
@@ -160,54 +156,54 @@ class FinalExpression{
         return total
     }
 
-    fun questionstr(): String {
-        return question_str
+    fun questionStr(): String {
+        return questionString
     }
 
 
 }
 
 class PrimaryExpression{
-    var total:Int =0
-    var question_str = ""
-    var operators= arrayOf("+","-","*","/")
-    var operatorstr =""
+    private var total:Int =0
+    private var questionString = ""
+    private var operators= arrayOf("+","-","*","/")
+    private var operatorStr =""
 
-    var qelement:Int =0
+    private var qElement:Int =0
 
-    fun primary_questionstr(): String {
-        operatorstr=operators.random()
+    fun primaryQuestionString(): String {
+        operatorStr=operators.random()
         val list = (1..20)
 
-        if (operatorstr.equals("/")){
+        if (operatorStr == "/"){
             val factors = list.shuffled()
             for(i in factors){
                 if(total%i==0){
-                    qelement= i
+                    qElement= i
                 }
             }
-        }else if (operatorstr.equals("+") || operatorstr.equals("*")){
+        }else if (operatorStr == "+" || operatorStr == "*"){
             val values = list.shuffled()
             for(i in values){
                 if(total+i<101 && total *i<101){
-                    qelement = i
+                    qElement = i
                     break
                 }
             }
 
         } else {
-            qelement = list.random()
+            qElement = list.random()
         }
-        question_str= "$operatorstr $qelement"
-        return question_str
+        questionString= "$operatorStr $qElement"
+        return questionString
     }
-    fun primary_questiontotal(finaltotal:Int): Int {
-        total = finaltotal
-        when(operatorstr){
-            "+" -> total=total+qelement
-            "-" -> total=total-qelement
-            "*" -> total=total*qelement
-            "/" -> total=total/qelement
+    fun primaryQuestionTotal(finalTotal:Int): Int {
+        total = finalTotal
+        when(operatorStr){
+            "+" -> total += qElement
+            "-" -> total -= qElement
+            "*" -> total *= qElement
+            "/" -> total /= qElement
 
         }
 
